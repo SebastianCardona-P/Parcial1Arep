560313dcaa6f30347155a55a529579a162bce416
unknown <sebastian.cardona-p@labinfo.is.escuelaing.edu.co>
2025-09-10 10:20:48 -0500

    primera configuración, falta readme arreglar división

---
+++ src/main/java/co/edu/escuelaing/parcial1corte/Facade.java
@@ -0,0 +1,175 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+package co.edu.escuelaing.parcial1corte;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
+
+/**
+ *
+ * @author Sebastian
+ */
+public class Facade {
+
+    private static final String USER_AGENT = "Mozilla/5.0";
+    private static final String GET_URL = "http://localhost:35000";
+    private static final int PORT = 36000;
+    private static String basePath = "src/main/java/resources/";
+
+    public static void main(String[] args) throws IOException, URISyntaxException {
+        ServerSocket serverSocket = null;
+        try {
+            serverSocket = new ServerSocket(PORT);
+        } catch (IOException e) {
+            System.err.println("Could not listen on port: " + PORT + ".");
+            System.exit(1);
+        }
+
+        boolean running = true;
+
+        while (running) {
+            Socket clientSocket = null;
+            try {
+                System.out.println("Listo para recibir ...");
+                clientSocket = serverSocket.accept();
+            } catch (IOException e) {
+                System.err.println("Accept failed.");
+                System.exit(1);
+            }
+
+            // IO stream
+            PrintWriter out = new PrintWriter(
+                    clientSocket.getOutputStream(), true);
+            BufferedReader in = new BufferedReader(
+                    new InputStreamReader(clientSocket.getInputStream()));
+            String inputLine, outputLine;
+
+            boolean isFirstLine = true;
+            URI path = null;
+
+            while ((inputLine = in.readLine()) != null) {
+
+                if (isFirstLine) {
+
+                    path = new URI(inputLine.split(" ")[1]);
+                    System.out.println("Path: " + path.getPath());
+                    isFirstLine = false;
+                }
+                System.out.println("Received: " + inputLine);
+                if (!in.ready()) {
+                    break;
+                }
+            }
+
+            handleRequest(path, out);
+
+            out.close();
+            in.close();
+            clientSocket.close();
+
+        }
+
+        serverSocket.close();
+    }
+
+    private static void handleRequest(URI path, PrintWriter out) throws IOException {
+        if (path.getPath().startsWith("/cliente") || path.getPath().equalsIgnoreCase("/")) {
+            getStaticFile(path, out);
+        } else if (path.getPath().startsWith("/add") || path.getPath().startsWith("/list")
+                || path.getPath().startsWith("/clear") || path.getPath().startsWith("/stats")) {
+            try {
+                connectionBack(path, out);
+            } catch (MalformedURLException ex) {
+                System.getLogger(Facade.class.getName()).log(System.Logger.Level.ERROR, (String) null, ex);
+            }
+        } else {
+            notFound(out);
+        }
+    }
+
+    private static void connectionBack(URI path, PrintWriter out) throws MalformedURLException, IOException {
+        String outputLine;
+        URL obj;
+
+        if(path.getPath().startsWith("/add")){
+            obj = new URL(GET_URL + path.getPath() + "?" + path.getQuery().replace(" ", ""));
+        } else{
+            obj = new URL(GET_URL + path.getPath());
+        }
+        
+        System.out.println("URL ESSSSS para el back" + obj.toString());
+        HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+        con.setRequestMethod("GET");
+        con.setRequestProperty("User-Agent", USER_AGENT);
+
+        int responseCode = con.getResponseCode();
+        System.out.println("GET Response Code :: " + responseCode);
+
+        if (responseCode == HttpURLConnection.HTTP_OK) {
+            BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+            String inputLine;
+            StringBuffer response = new StringBuffer();
+            while ((inputLine = in.readLine()) != null) {
+                response.append(inputLine);
+            }
+            in.close();
+            outputLine = "HTTP/1.1 200 OK\n\r"
+                    + "contente-type: application/json\n\r"
+                    + "\n\r";
+            outputLine += response.toString();
+        } else {
+            System.out.println("GET request not worked");
+            outputLine = "HTTP/1.1 502 OK\n\r"
+                    + "contente-type: application/json\n\r"
+                    + "\n\r";
+            outputLine += "{\"status\": \"ERR\", \"error\": \"backend_unreachable\"}";
+        }
+        out.write(outputLine);
+    }
+
+    private static void getStaticFile(URI requestUri, PrintWriter out) {
+        String outputLine = "HTTP/1.1 200 OK\n\r"
+                + "contente-type: text/html\n\r"
+                + "\n\r";
+
+        // create the file path
+        String file = basePath + "index.html";
+        File realFile = new File(file);
+        if (!realFile.exists() && !realFile.isFile()) {
+            notFound(out);
+            return;
+        }
+
+        // start reading the file
+        try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
+            String fileLine;
+            while ((fileLine = reader.readLine()) != null) {
+                outputLine += fileLine + "\n";
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        out.write(outputLine);
+    }
+
+    private static void notFound(PrintWriter out) {
+        String response = "HTTP/1.1 404 Not Found\r\n"
+                + "Content-Type: text/plain\r\n"
+                + "\r\n"
+                + "404 Not Found";
+        out.write(response);
+    }
+}
---
+++ src/main/java/co/edu/escuelaing/parcial1corte/HttpServer.java
@@ -0,0 +1,199 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package co.edu.escuelaing.parcial1corte;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
+/**
+ *
+ * @author Sebastian
+ */
+public class HttpServer {
+
+    private static final int PORT = 35000;
+    private static List<Integer> listStorage = new LinkedList<>();
+    private static int countList = 0;
+
+    public static void main(String[] args) throws IOException, ClassNotFoundException, InvocationTargetException, NoSuchMethodException, IllegalAccessException, URISyntaxException {
+        ServerSocket serverSocket = null;
+        try {
+            serverSocket = new ServerSocket(PORT);
+        } catch (IOException e) {
+            System.err.println("Could not listen on port: " + PORT + ".");
+            System.exit(1);
+        }
+
+        boolean running = true;
+
+        while (running) {
+            Socket clientSocket = null;
+            try {
+                System.out.println("Listo para recibir ...");
+                clientSocket = serverSocket.accept();
+            } catch (IOException e) {
+                System.err.println("Accept failed.");
+                System.exit(1);
+            }
+
+            // IO stream
+            PrintWriter out = new PrintWriter(
+                    clientSocket.getOutputStream(), true);
+            BufferedReader in = new BufferedReader(
+                    new InputStreamReader(clientSocket.getInputStream()));
+            String inputLine, outputLine;
+
+            boolean isFirstLine = true;
+            URI path = null;
+
+            while ((inputLine = in.readLine()) != null) {
+
+                if (isFirstLine) {
+
+                    path = new URI(inputLine.split(" ")[1]);
+                    System.out.println("Path: " + path.getPath());
+                    isFirstLine = false;
+                }
+                System.out.println("Received: " + inputLine);
+                if (!in.ready()) {
+                    break;
+                }
+            }
+
+            handleRequest(path, out);
+
+            out.close();
+            in.close();
+            clientSocket.close();
+
+        }
+
+        serverSocket.close();
+    }
+
+    private static void handleRequest(URI path, PrintWriter out) throws ClassNotFoundException, NoSuchMethodException, IllegalAccessException, InvocationTargetException {
+        if (path.getPath().startsWith("/add")) {
+            System.out.println("Voy para el add");
+            handleAdd(path, out);
+        } else if (path.getPath().startsWith("/list")) {
+            handleList(path, out);
+            System.out.println("Me fui para list");
+        } else if (path.getPath().startsWith("/clear")) {
+            handleClear(path, out);
+            System.out.println("Me fui para clear");
+        } else if (path.getPath().startsWith("/stats")) {
+            handleStats(path, out);
+            System.out.println("Me fui para stats");
+        } else {
+            System.out.println("No enconte petición");
+            notSoported(out);
+        }
+    }
+
+    private static void notSoported(PrintWriter out) {
+        String response = "HTTP/1.1 404 Not Found\r\n"
+                + "Content-Type: application/json\r\n"
+                + "\r\n"
+                + "{\"message\": \"This method is not soported yet\"}";
+        out.write(response);
+    }
+
+    private static void handleAdd(URI path, PrintWriter out) {
+        String response = "HTTP/1.1 200 OK\r\n"
+                + "Content-Type: application/json\r\n"
+                + "\r\n";
+        System.out.println("mi query es:" + path.getQuery());
+        String stringReal = path.getQuery().split("=")[1].replace(" ", "");
+        System.out.println("mi real es:" + stringReal);
+
+        int real = Integer.parseInt(stringReal);
+
+        listStorage.add(real);
+        countList = listStorage.size();
+        System.out.println("mi lista es" + listStorage.toString());
+
+        response += "{\"status\": \"OK\", \"added\": \"" + real + "\"," + "\"count\": \"" + countList + "\"" + "}";
+
+        out.write(response);
+    }
+
+    private static void handleList(URI path, PrintWriter out) {
+        String response = "HTTP/1.1 200 OK\r\n"
+                + "Content-Type: application/json\r\n"
+                + "\r\n";
+
+        String listString = listStorage.toString();
+
+        response += "{\"status\": \"OK\", \"values\": \"" + listString + "\"," + "\"count\": \"" + countList + "\"" + "}";
+
+        out.write(response);
+    }
+
+    private static void handleClear(URI path, PrintWriter out) {
+        String response = "HTTP/1.1 200 OK\r\n"
+                + "Content-Type: application/json\r\n"
+                + "\r\n";
+
+        listStorage.clear();
+        countList = listStorage.size();
+
+        response += "{\"status\": \"OK\", \"message\": \"list_cleared\"" + "}";
+
+        out.write(response);
+    }
+
+    private static void handleStats(URI path, PrintWriter out) {
+        String response;
+        if (listStorage.isEmpty()) {
+            response = "HTTP/1.1 409 OK\r\n"
+                    + "Content-Type: application/json\r\n"
+                    + "\r\n"
+                    + "{\"status\": \"ERR\", \"error\": \"empty_list\"" + "}";
+        } else {
+            response = "HTTP/1.1 200 OK\r\n"
+                    + "Content-Type: application/json\r\n"
+                    + "\r\n";
+
+            countList = listStorage.size();
+
+            int totalSum = 0;
+            for (Integer num : listStorage) {
+                totalSum += num;
+            }
+            
+            System.out.println("total sum: " + totalSum);
+            System.out.println("total lista: "+ countList);
+            double mean = totalSum / countList;
+            
+            System.out.println("mean es: "+ mean);
+
+            double totalDiferencia = 0;
+            for (Integer num : listStorage) {
+                totalDiferencia += Math.pow((double) num - mean, 2);
+            }
+
+            double desv = Math.sqrt((double) (1 / (countList - 1) * totalDiferencia));
+
+            response += "{\"status\": \"OK\", \"mean\": \"" + mean + "\"," + "\"stddev\": \"" + desv + "\"," + "\"count\": \"" + countList + "\"" + "}";
+        }
+
+        out.write(response);
+    }
+
+}
